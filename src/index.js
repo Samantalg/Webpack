1713ebab6ca13df9109d238dd32c6a285cf2694f
Samanta <slaragiannone@gmail.com>
2018-10-19 19:39:33 +0100

    babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import bar from './bar'; //no se pone {bar} porque es por defecto
+import bar, { defaultParameters } from './bar'; //no se pone {bar} porque es por defecto
 
 bar();
+defaultParameters(100,'#OOOOFF');
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,17 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js', //empieza a leer en el index.js
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
     output: {
         path: path.resolve(__dirname, 'dist'), //crea la carpeta dist y lo mete en bundle.js
         filename: 'bundle.js'
